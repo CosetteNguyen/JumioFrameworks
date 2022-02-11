c5fe64f2fe2f69f415c2387b5f1f9f5a7063911d
Cosette Nguyen <anh.nguyen8@sotatek.com>
2022-02-11 15:30:39 +0700

    add files

---
+++ Jumio Package/Jumio_Package.h
@@ -0,0 +1,18 @@
+//
+//  Jumio_Package.h
+//  Jumio Package
+//
+//  Created by Cosette Nguyen on 11/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jumio_Package.
+FOUNDATION_EXPORT double Jumio_PackageVersionNumber;
+
+//! Project version string for Jumio_Package.
+FOUNDATION_EXPORT const unsigned char Jumio_PackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jumio_Package/PublicHeader.h>
+
+
